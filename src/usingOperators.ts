58c423a0d5092c470fecf8557da2cb84128447d6
Leon Yalin <yelinichevl@gmail.com>
2019-08-21 22:29:34 +0300

    using operators part 3; finish

--- src/usingOperators.ts
+++ src/usingOperators.ts
@@ -1,16 +1,23 @@
 import { ajax } from 'rxjs/ajax';
-import { filter, map, mergeMap, take, tap } from 'rxjs/operators';
+import { filter, map, mergeMap, take, tap, catchError, takeUntil } from 'rxjs/operators';
 import { delimeterMsg, logF } from "./utils";
-import { from } from 'rxjs';
+import { from, of, throwError, fromEvent } from 'rxjs';
 
 function creatingOperators() {
+  const btn$ = fromEvent(document.getElementById('fromEvent'), 'click');
+  
   ajax('https://jsonplaceholder.typicode.com/posts/')
-  .pipe(
-    mergeMap(ajaxData => from(ajaxData.response).pipe(take(5))),
-    filter(item => item['id'] === 1),
-    tap(item => console.log('tap', item)),
-  )
-  .subscribe(data => console.log('data after pipe', data))
+    .pipe(
+      mergeMap(ajaxData => from(ajaxData.response).pipe(take(5))),
+      filter(item => item['id'] === 1),
+      tap(item => console.log('tap', item)),
+      takeUntil(btn$), /* will stop when btn$ will emit a value */
+      // catchError(err => of({firstName: 'Leon', lastName: 'Yalin'}) /* will not go to subscribe error callback */
+      // catchError((err, caught) => caught)
+      // catchError(err => { throw new Error(err) })
+      catchError(err => { return throwError(err) })
+    )
+    .subscribe(data => console.log('data after pipe', data))
 }
 
 export default function usingOperators() {
