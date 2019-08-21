d447c816b3f4b3fcb4cf13c90f9373513a602864
Leon Yalin <yelinichevl@gmail.com>
2019-08-21 22:38:02 +0300

    creating custom operators part 1

---
+++ src/creatingCustomOperators.ts
@@ -0,0 +1,10 @@
+import { delimeterMsg, logF } from "./utils";
+
+function creatingCustomOperators() {
+  
+}
+
+export default function createCustomOperators() {
+  delimeterMsg('CREATING CUSTOM OPERATORS');
+  logF(creatingCustomOperators);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import creatingObservables from "./creatingObservables";
 import subscribingToObservables from "./subscribingToObservables";
 import usingOperators from "./usingOperators";
+import createCustomOperators from "./creatingCustomOperators";
 
 (function main() {
   creatingObservables();
   subscribingToObservables();
   usingOperators();
+  createCustomOperators();
 }());
